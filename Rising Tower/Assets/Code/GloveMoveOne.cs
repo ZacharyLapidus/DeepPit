2b133cf84992e828eacc645eb127178d4d631d85
ZacharyLapidus <44439798+ZacharyLapidus@users.noreply.github.com>
2019-11-13 15:42:57 -0500

    Added death code for punching glove

---
+++ Rising Tower/Assets/Code/GloveMoveOne.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GloveMoveOne : MonoBehaviour
+{
+    private Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        {
+            Debug.Log("Move");
+            transform.Translate(0f, 0f, -3f);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        {
+            Debug.Log("Move");
+            transform.Translate(0f, 0f, 3f);
+        }
+    }
+}
