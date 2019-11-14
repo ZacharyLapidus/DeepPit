50e29548e5e9e322eabe8bafc8757df410fc06df
ZacharyLapidus <44439798+ZacharyLapidus@users.noreply.github.com>
2019-11-14 11:47:18 -0500

    Started Dropper Code

---
+++ Rising Tower/Assets/Code/Trap.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public float xAngle, yAngle, Zanglel
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*IEnumerator Dropper()
+    {
+        WaitForSeconds(1.5);
+        transform.position = new Vector3(0f, 0f, 0f);
+        
+    }*/
+    void OnTriggerEnter(Collider other)
+    {
+        {
+            Debug.Log("Drop");
+            //StartCoroutine(Dropper));
+            //transform.localScale += new Vector3(x, y, z);
+        }
+    }
+
+    /*void OnTriggerExit(Collider other)
+    {
+        {
+            Debug.Log("Move");
+            transform.localScale -= new Vector3(x, y, z);
+        }
+    }*/
+}
