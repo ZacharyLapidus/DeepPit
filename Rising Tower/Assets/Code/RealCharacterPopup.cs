ad57b33276c150249653d7b813253e61a3d30198
ZacharyLapidus <44439798+ZacharyLapidus@users.noreply.github.com>
2019-11-15 09:54:00 -0500

    Added a bit of audio though its not very nice

---
+++ Rising Tower/Assets/Code/RealCharacterPopup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class RealCharacterPopup : MonoBehaviour
+{
+    public float x = 0f;
+    public float y = 0f;
+    public float z = 0f;
+    public float xdown = 0f;
+    public float ydown = 0f;
+    public float zdown = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        {
+            Debug.Log("I found you faker!");
+            transform.Translate(x, y, z);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        {
+            Debug.Log("You're not even good enough to be my fake");
+            transform.Translate(xdown, ydown, zdown);
+        }
+    }
+}
--- Rising Tower/Assets/Code/SpringExpand.cs
+++ Rising Tower/Assets/Code/SpringExpand.cs
@@ -8,10 +8,13 @@ public class SpringExpand : MonoBehaviour
     public float x = 0f;
     public float y = .5f;
     public float z = 0f;
+    private AudioSource m_boioing;
+    private AudioClip boioing;
     // Start is called before the first frame update
     void Start()
     {
         startPos = this.transform.position;
+        m_boioing = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,8 +27,16 @@ public class SpringExpand : MonoBehaviour
         {
             Debug.Log("Move");
             transform.localScale += new Vector3(x, y, z);
+            PlayBoioing();
+            
         }
     }
+    private void PlayBoioing()
+    {
+        m_boioing.clip = boioing;
+        m_boioing.Play();
+    }
+
     void OnTriggerExit(Collider other)
     {
         {
